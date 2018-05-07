5d3b3bbad85cdd0d276fabc2a2f433b7c918b1d6
Rico Sharp <ricosharp@ricomac.studio.ricosharp.com>
2018-05-06 20:14:58 -0400

    Enable theme features and added first post

---
+++ _posts/2018/2018-05-06-My-First-Jekyll-Post.md
@@ -0,0 +1,5 @@
+---
+title: "My First Jekyll Post"
+---
+
+Hello World!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "My First Jekyll Post",
+        "excerpt":"Hello World! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/My-First-Jekyll-Post",
+        "teaser":null}]
